cf651b66f73461dff761b905680698a9525a2e5a
MellKay <59118345+MellKay@users.noreply.github.com>
2020-11-30 14:09:46 +0000

    Initialize basic-implementation

---
+++ index.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (message) => {
+    io.emit('chat message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
